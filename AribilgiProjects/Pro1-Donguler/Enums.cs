dff44544c39512a93307a967069a3051c082f34f
kpamukcu <kapawebtasarim@gmail.com>
2022-06-25 14:58:39 +0300

    Döngü komutları gönderildi.

---
+++ AribilgiProjects/Pro1-Donguler/Enums.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pro1_Donguler
+{
+    enum Rol
+    {
+        Admin = 1,
+        Musteri = 2,
+        Danisman = 3
+    }
+}
--- AribilgiProjects/Pro1-Donguler/Program.cs
+++ AribilgiProjects/Pro1-Donguler/Program.cs
@@ -10,7 +10,54 @@ namespace Pro1_Donguler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Merhaba Sedat");
+            #region Örnek 1
+            //Console.Write("Kullanici Adı=");
+            //string kullaniciAdi = Console.ReadLine();
+            //Console.Write("Sifre = ");
+            //string Sifre = Console.ReadLine();
+            //Rol rolu = default(Rol);
+
+            //if (kullaniciAdi == "cem" && Sifre == "1234")
+            //    rolu = Rol.Admin;
+            //else if (kullaniciAdi == "A" && Sifre == "1234")
+            //    rolu = Rol.Musteri;
+            //else if (kullaniciAdi == "B" && Sifre == "1234")
+            //    rolu = Rol.Danisman;
+            //else
+            //    rolu = default(Rol);
+
+            //switch (rolu)
+            //{
+            //    case Rol.Admin:
+            //        Console.WriteLine("Admin Paneline hoşgeldiniz");
+            //        break;
+            //    case Rol.Musteri:
+            //        Console.WriteLine("Musteri Paneline hoşgeldiniz");
+            //        break;
+            //    case Rol.Danisman:
+            //        Console.WriteLine("Danısman Paneline hoşgeldiniz");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Yetkisiz Giris");
+            //        break;
+            //}
+            #endregion
+
+            #region ÖRnek 2
+            //for (int sayi = 0; sayi <= 10; sayi++)
+            //{
+            //    Console.WriteLine(sayi);
+            //}
+            #endregion
+
+            #region Örnek 3
+            //Sonsuz Döngü
+            //for(;true; )
+            //{
+            //    Console.WriteLine("Merhaba Dünya");
+            //}
+            #endregion
+
         }
     }
 }
